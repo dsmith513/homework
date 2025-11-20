99f160510a60bdf60887cdd92def89a3568c33a1
Dylan Smith <dylan.smith513@gmail.com>
2025-11-20 13:08:49 -0600

    assignment9

--- stat382/homework-10/homework-10.R
+++ stat382/homework-10/homework-10.R
@@ -53,3 +53,125 @@ table(nfl2016$Side)
 # (c) Specifically state the p-value.
 # (d) State your decision(s) (Reject H0 or Do Not Reject H0).
 # (e) State your conclusion(s).
+
+# (a) H0: The residuals are normally distributed.
+#     H1: The residuals are not normally distributed.
+
+# (b) 
+shapiro.test(nfl2016_model$residuals)
+# Shapiro-Wilk normality test
+# 
+# data:  nfl2016_model$residuals
+# W = 0.98015, p-value < 2.2e-16
+
+# (c) p < 2.2e-16
+
+# (d) Reject H0. p < 2.2e-16 < 0.03 = alpha
+
+# (e) There is significant evidence to conclude that the residuals are not normally distributed.
+#     The normality of residuals assumption is not met.
+
+
+# 8. Check the Homogeneity of Variance Assumption by running the most appropriate of
+# Bartlett’s Test or Levene’s Test using a significance level of 3%.
+# Be sure to:
+# (a) State your hypotheses using symbols.
+# (b) Copy / paste your test results into your code.
+# (c) Specifically state the p-value.
+# (d) State your decision (Reject H0 or Do Not Reject H0).
+# (e) State your conclusion.
+
+# (a) H0: sigma_def^2 = sigma_off^2 = sigma_st^2
+#     H1: At least one sigma^2 differs
+
+# (b)
+library(car)
+leveneTest(Height ~ Side, data = nfl2016)
+# Levene's Test for Homogeneity of Variance (center = median)
+#         Df F value    Pr(>F)    
+# group    2  36.815 < 2.2e-16 ***
+#       2761                      
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# (c) p < 2.2e-16
+
+# (d) Reject H0. p < 2.2e-16 < 0.03 = alpha
+
+# (e) There is significant evidence to conclude that the player height variances are not equal.
+
+
+# 9. Provide the ANOVA table for the overall model. Copy / paste your table into your code.
+
+anova(nfl2016_model)
+# Analysis of Variance Table
+# 
+# Response: Height
+# Df  Sum Sq Mean Sq F value    Pr(>F)    
+# Side         2   704.1  352.06  51.731 < 2.2e-16 ***
+#   Residuals 2761 18790.4    6.81                      
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
+# 10. From the ANOVA table generated in Question 9, does it appear that there is a difference
+# between sides using α = 0.03?
+#   Be sure to:
+# (a) State the hypotheses using symbols. Use the treatment effects model.
+# (b) State the value of the test statistic. Indicate whether you are using F or t.
+# (c) State the p-value.
+# (d) What decision (Reject H0 or Do Not Reject H0) do you make?
+# (e) What conclusion can you make?
+
+# (a) H0: μ_def = μ_off = μ_st
+#     H1: At least one μ differs
+
+# (b) F = 51.731
+
+# (c) p < 2.2e-16
+
+# (d) Reject H0. p < 2.2e-16 < 0.03 = alpha
+
+# (e) There is significant evidence that the mean player heights are not equal.
+
+
+# 11. Perform the Bonferroni Test for Multiple Pairwise Comparisons using α = 0.03.
+#  Provide your code. Copy / paste your results into your code.
+#  For each group, state whether the difference was significant or not.
+
+pairwise.t.test(x = nfl2016$Height, g = nfl2016$Side, p.adj = "bonferroni")
+# Pairwise comparisons using t tests with pooled SD 
+# 
+# data:  nfl2016$Height and nfl2016$Side 
+# 
+# DEF     OFF    
+# OFF < 2e-16 -      
+#   ST  0.98    3.6e-06
+# 
+# P value adjustment method: bonferroni
+
+# For DEF and OFF, the average heights differ significantly.
+# For DEF and ST, the average heights do not differ significantly.
+# For OFF and ST, the average heights differ significantly.
+
+
+# 12. Find the Tukey Simultaneous Confidence Intervals for all pairwise differences using α =
+#   0.03.
+#  Provide your code. Copy / paste your results into your code.
+#  For each group, state whether the difference was significant or not.
+
+TukeyHSD(aov(nfl2016_model), conf.level = 0.97)
+# Tukey multiple comparisons of means
+# 97% family-wise confidence level
+# 
+# Fit: aov(formula = nfl2016_model)
+# 
+# $Side
+# diff        lwr        upr     p adj
+# OFF-DEF  0.9849343  0.7274753  1.2423934 0.0000000
+# ST-DEF  -0.2491754 -0.8935851  0.3952343 0.5883628
+# ST-OFF  -1.2341097 -1.8777618 -0.5904577 0.0000035
+
+# For DEF and OFF, the average heights differ significantly.
+# For DEF and ST, the average heights do not differ significantly.
+# For OFF and ST, the average heights differ significantly.
